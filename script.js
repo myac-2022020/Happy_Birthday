fdad5f259c175006fe11d39eee81cabdeb7f30ad
myac-2022020@kinal.edu.gt <myac-2022020@kinal.edu.gt>
2025-05-02 01:29:57 -0600

    Se subio albun de fotos

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('verGaleria').addEventListener('click', function(){
+    document.getElementById('contenedorGaleria').style.display = 'block'
+    document.getElementById('finalGalery').style.display = 'block'
+    this.style.display = 'none'
+})
