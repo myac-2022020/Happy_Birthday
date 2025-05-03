b6530b09209e899c620c03557a93cbb0bcd8ccd3
myac-2022020@kinal.edu.gt <myac-2022020@kinal.edu.gt>
2025-05-03 02:34:34 -0600

    Regalo listo

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 document.getElementById('verGaleria').addEventListener('click', function(){
     document.getElementById('contenedorGaleria').style.display = 'block'
     document.getElementById('finalGalery').style.display = 'block'
+    document.getElementById('principalText').style.display = 'none'
     this.style.display = 'none'
 })
+
+const velasContainer = document.getElementById('velasContainer')
+
+for (let i = 0; i < 16; i++) {
+  const vela = document.createElement('div')
+  vela.classList.add('vela')
+
+  const mecha = document.createElement('div')
+  mecha.classList.add('mecha')
+
+  const llama = document.createElement('div')
+  llama.classList.add('llama')
+
+  vela.appendChild(mecha)
+  vela.appendChild(llama)
+
+  vela.addEventListener('click', () => {
+    if (llama) {
+      llama.remove()
+
+      const humo = document.createElement('div')
+      humo.classList.add('humo')
+      vela.appendChild(humo)
+    }
+  })
+
+  velasContainer.appendChild(vela)
+}
